6c81c86ad7d748fabf310c83ccec2b7ce647f8dc
LUZAmaxxp <allouchayman21@gmail.com>
2026-01-09 09:44:40 +0100

    feat : dockerizing and deploying it to vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/auth_next';
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/auth_next';
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+if (!MONGODB_URI.startsWith('mongodb://') && !MONGODB_URI.startsWith('mongodb+srv://')) {
+  MONGODB_URI = 'mongodb://localhost:27017/auth_next';
 }
 
 /**
